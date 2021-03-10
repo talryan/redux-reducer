75db551f297b27a9157f33b7f94074ca2cdecedf
talryan <nataliamryan@gmail.com>
2021-03-09 18:43:39 -0800

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,10 +1,31 @@
-export function manageFriends(state, action){
-  switch (action.type){
-      case 'ADD_FRIEND':
-          return {...state, friends: [state.friends, action.friend]}
-      case 'REMOVE_FRIEND':
-          return {friends: state.friends.filter(friend => friend.id !== action.id)}
+export function manageFriends(state = {
+    friends: [],
+  }, action) {
+    switch(action.type) {
+  
+      case "ADD_FRIEND":
+        return (
+          {...state,
+              friends: [
+                ...state.friends,
+                action.friend
+              ]
+          }
+      )
+  
+      case "REMOVE_FRIEND":
+        const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+        return (
+          {...state,
+              friends: [
+                ...state.friends.slice(0, removalIndex),
+                ...state.friends.slice(removalIndex + 1)
+              ]
+          }
+        )
+  
       default:
-          return state;
-  }
-}
+        return state;
+  
+    }
+  };
